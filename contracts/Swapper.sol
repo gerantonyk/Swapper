5f4e81079b8f621323711713ce9ca6ae4e46b934
gerantonyk <ger.antonyk@gmail.com>
2021-09-28 23:29:27 -0300

    withdraw function + unit test

--- contracts/Swapper.sol
+++ contracts/Swapper.sol
@@ -20,19 +20,26 @@ contract Swapper {
         toToken   = _toToken;
     }
 
-    function provide(uint _amount) public returns(bool) {
+    function provide(uint _amount) public  {
         require(_amount > 0, "Provided amount can't be 0");
-        uint256 allowance = fromToken.allowance(msg.sender, address(this));
-        require(allowance >= _amount, "Amount exceeds allowance");
+        uint256 _allowance = fromToken.allowance(msg.sender, address(this));
+        require(_allowance >= _amount, "Amount exceeds allowance");
         providedByAddress[msg.sender] = providedByAddress[msg.sender].add(_amount);
-        fromToken.transferFrom(msg.sender, address(this), _amount);
-        return true;
+        fromToken.transferFrom(msg.sender, address(this), _amount);  
     }
 
-    function swap() public returns(bool) {
+    function swap() public  {
         require(providedByAddress[msg.sender]>0,"Address didn't provide tokens");
         swappedByAddress[msg.sender]= providedByAddress[msg.sender].mul(exchangeRatio);
         providedByAddress[msg.sender] = 0;
-        return true;
     }
+
+    function withdraw() public  {
+        require(swappedByAddress[msg.sender]>0,"There are no tokens to withdraw");
+        uint256 _balance = toToken.balanceOf(address(this));
+        uint  _swapped = swappedByAddress[msg.sender];
+        require(_balance >= _swapped, "There is not enough tokens");
+        swappedByAddress[msg.sender] = 0;
+        toToken.transfer(msg.sender,_swapped);
+    }    
 }
--- test/unittest.js
+++ test/unittest.js
@@ -25,6 +25,8 @@ describe("Swapper", function () {
     it('Should revert if greeting is amount exeeds allowance', async function () {
       await token1.approve(swapper.address,500)
       await expect(swapper.provide(700)).to.be.revertedWith("Amount exceeds allowance")
+      let providedAmount = await swapper.providedByAddress(owner.address)
+      expect(providedAmount.toNumber()).to.be.equal(0)
     });
     it('Should transfer the token to Swapper if amount is equal or less tha allowance', async function () {
       await token1.approve(swapper.address,600)
@@ -47,28 +49,57 @@ describe("Swapper", function () {
   describe('Swap function', function() {
 
     it("Should revert if provided amount is 0", async function () {
-      await expect(swapper.swap).to.be.revertedWith("Address didn't provide tokens")
+      await expect(swapper.swap()).to.be.revertedWith("Address didn't provide tokens")
     });
-    xit('Should revert if greeting is amount exeeds allowance', async function () {
-      await token1.approve(swapper.address,500)
-      await expect(swapper.provide(700)).to.be.revertedWith("Amount exceeds allowance")
-    });
-    xit('Should transfer the token to Swapper if amount is equal or less tha allowance', async function () {
-      await token1.approve(swapper.address,600)
-      await swapper.provide(600)
-      let swapperBalance = await token1.balanceOf(swapper.address)
+    it('Should swap provided amount', async function () {
+      await token1.approve(swapper.address,700)
+      await swapper.provide(700)
+      await swapper.swap()
       let providedAmount = await swapper.providedByAddress(owner.address)
-      expect(providedAmount.toNumber()).to.be.equal(600)
-      expect(swapperBalance.toNumber()).to.be.equal(600)
+      let swappeddAmount = await swapper.swappedByAddress(owner.address)
+      expect(providedAmount.toNumber()).to.be.equal(0)
+      expect(swappeddAmount.toNumber()).to.be.equal(700)
+    });
+    it('Should only swap tokens from the caller', async function () {
+      await token1.transfer(addr1.address,5000)
+      await token1.approve(swapper.address,700)
+      await swapper.provide(700)
+      await token1.connect(addr1).approve(swapper.address,1500)
+      await swapper.connect(addr1).provide(1000)
+      await swapper.swap()
+      let providedAmount = await swapper.providedByAddress(owner.address)
+      let swappeddAmount = await swapper.swappedByAddress(owner.address)
+      expect(providedAmount.toNumber()).to.be.equal(0)
+      expect(swappeddAmount.toNumber()).to.be.equal(700)
+      providedAmount = await swapper.providedByAddress(addr1.address)
+      swappeddAmount = await swapper.swappedByAddress(addr1.address)
+      expect(providedAmount.toNumber()).to.be.equal(1000)
+      expect(swappeddAmount.toNumber()).to.be.equal(0)      
     });    
-    xit('Should sum all the amounts provided', async function () {
-      await token1.approve(swapper.address,900)
-      await swapper.provide(500)
-      await swapper.provide(300)
-      let swapperBalance = await token1.balanceOf(swapper.address)
-      let providedAmount = await swapper.providedByAddress(owner.address)
-      expect(providedAmount.toNumber()).to.be.equal(800)
-      expect(swapperBalance.toNumber()).to.be.equal(800)
-    }); 
   });  
+  describe('Withdraw function', function() {
+
+    it("Should revert if swapped amount is 0", async function () {
+      await expect(swapper.withdraw()).to.be.revertedWith("There are no tokens to withdraw")
+    });
+    it('Should revert if there is not enough tokens to withdraw', async function () {
+      await token1.approve(swapper.address,700)
+      await swapper.provide(700)
+      await swapper.swap()
+      await expect(swapper.withdraw()).to.be.revertedWith("There is not enough tokens")
+      let swappeddAmount = await swapper.swappedByAddress(owner.address)
+      expect(swappeddAmount.toNumber()).to.be.equal(700)
+    });
+    it('Should tranfer the swapped amount to the owner', async function () {
+      await token2.transfer(swapper.address,5000)
+      await token1.approve(swapper.address,700)
+      await swapper.provide(700)
+      await swapper.swap()
+      await swapper.withdraw()
+      let swappeddAmount = await swapper.swappedByAddress(owner.address)
+      expect(swappeddAmount.toNumber()).to.be.equal(0)
+      let balance = await token2.balanceOf(swapper.address)
+      expect(balance.toNumber()).to.be.equal(4300)
+    });    
+  });    
 });
